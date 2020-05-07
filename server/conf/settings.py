739b075818f24102dfd26f26814bf4ae76a9fb81
devinMcArthur <assortedgerm@gmail.com>
2020-05-07 09:25:55 -0600

    Static 1.2

--- server/conf/settings.py
+++ server/conf/settings.py
@@ -121,6 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+MIDDLEWARE_CLASSES = (
+    'whitenoise.middleware.WhiteNoiseMiddleware'
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
