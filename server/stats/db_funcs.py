3a74be7445c84879de0d6b50b734ca303955f262
devinMcArthur <assortedgerm@gmail.com>
2020-05-06 18:37:11 -0600

    DB 1.15

--- server/stats/db_funcs.py
+++ server/stats/db_funcs.py
@@ -10,9 +10,8 @@ def update_stats():
     # XXX This query works on sqlite only!
 
     with connection.cursor() as cursor:
-        print("HI")
-        print(cursor)
         cursor.execute('DELETE FROM "stats_messagegrid";')
+        # WE ARE USING POSTGRESQL (SQLITE is not intended for PRODUCTION)
         # cursor.execute("""
         #     INSERT INTO "stats_messagegrid"(channel_id, day_of_week, hour, count)
         #     SELECT
@@ -28,47 +27,51 @@ def update_stats():
 def get_server_mph_by_dow(server):
     "Get messages-per-hour by day-of-week"
     with connection.cursor() as cursor:
-        cursor.execute("""
-        SELECT [day_of_week]+1, avg([count])
-        FROM [stats_messagegrid] m
-        JOIN [stats_channel] c ON m.channel_id = c.disc_id
-        WHERE c.server_id = %s
-        GROUP BY [day_of_week]
-        """, [server.disc_id])
+        # WE ARE USING POSTGRESQL (SQLITE is not intended for PRODUCTION)
+        # cursor.execute("""
+        # SELECT [day_of_week]+1, avg([count])
+        # FROM [stats_messagegrid] m
+        # JOIN [stats_channel] c ON m.channel_id = c.disc_id
+        # WHERE c.server_id = %s
+        # GROUP BY [day_of_week]
+        # """, [server.disc_id])
         return dict(cursor.fetchall())
 
 
 def get_server_mph_by_hod(server):
     "Get messages-per-hour by hour of day"
     with connection.cursor() as cursor:
-        cursor.execute("""
-        SELECT [hour], avg([count])
-        FROM [stats_messagegrid] m
-        JOIN [stats_channel] c ON m.channel_id = c.disc_id
-        WHERE c.server_id = %s
-        GROUP BY [hour]
-        """, [server.disc_id])
+        # WE ARE USING POSTGRESQL (SQLITE is not intended for PRODUCTION)
+        # cursor.execute("""
+        # SELECT [hour], avg([count])
+        # FROM [stats_messagegrid] m
+        # JOIN [stats_channel] c ON m.channel_id = c.disc_id
+        # WHERE c.server_id = %s
+        # GROUP BY [hour]
+        # """, [server.disc_id])
         return dict(cursor.fetchall())
 
 def get_channel_mph_by_dow(channel):
     "Get messages-per-hour by day-of-week"
     with connection.cursor() as cursor:
-        cursor.execute("""
-        SELECT [day_of_week]+1, avg([count])
-        FROM [stats_messagegrid]
-        WHERE channel_id = %s
-        GROUP BY [day_of_week]
-        """, [channel.disc_id])
+        # WE ARE USING POSTGRESQL (SQLITE is not intended for PRODUCTION)
+        # cursor.execute("""
+        # SELECT [day_of_week]+1, avg([count])
+        # FROM [stats_messagegrid]
+        # WHERE channel_id = %s
+        # GROUP BY [day_of_week]
+        # """, [channel.disc_id])
         return dict(cursor.fetchall())
 
 
 def get_channel_mph_by_hod(channel):
     "Get messages-per-hour by hour of day"
     with connection.cursor() as cursor:
-        cursor.execute("""
-        SELECT [hour], avg([count])
-        FROM [stats_messagegrid]
-        WHERE channel_id = %s
-        GROUP BY [hour]
-        """, [channel.disc_id])
+        # WE ARE USING POSTGRESQL (SQLITE is not intended for PRODUCTION)
+        # cursor.execute("""
+        # SELECT [hour], avg([count])
+        # FROM [stats_messagegrid]
+        # WHERE channel_id = %s
+        # GROUP BY [hour]
+        # """, [channel.disc_id])
         return dict(cursor.fetchall())
