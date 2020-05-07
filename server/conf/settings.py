0ada2738a8151d97b7429c3deb8b3d7dffc6bd11
devinMcArthur <assortedgerm@gmail.com>
2020-05-07 09:21:33 -0600

    Static 1.1

--- server/conf/settings.py
+++ server/conf/settings.py
@@ -121,9 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-STATIC_ROOT = '../stats/templates/index.html'
 
 DISCORD_TOKEN = None
 
