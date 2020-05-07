8fafc2b06e7dc7eebd1348837f12e35d630e6509
devinMcArthur <assortedgerm@gmail.com>
2020-05-07 09:54:35 -0600

    Static 1.11

--- server/conf/settings.py
+++ server/conf/settings.py
@@ -128,7 +128,7 @@ MIDDLEWARE_CLASSES = (
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
