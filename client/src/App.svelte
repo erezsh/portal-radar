c537535888c61ac4f040f9c2eef9071451d12870
Erez Sh <erezshin@gmail.com>
2020-05-07 10:01:47 +0300

    Client initial

---
+++ client/src/App.svelte
@@ -0,0 +1,283 @@
+<script>
+	import { afterUpdate, onMount } from 'svelte';
+	import Graph from './Graph.svelte';
+
+	let server_list = []
+	let channels_dict = {}
+
+	async function list_servers() {
+		let r = await fetch('http://127.0.0.1:8000/servers')
+
+		if (!r.ok) {
+			console.log("HTTP-Error: " + r.status);
+			return {}
+		}
+
+		let json = await r.json()
+		return Object.values(json);
+	}
+
+	async function list_channels(server) {
+		let r = await fetch('http://127.0.0.1:8000/channels/' + server)
+
+		if (!r.ok) {
+			console.log("HTTP-Error: " + r.status);
+			return {}
+		}
+
+		let json = await r.json()
+		return Object.values(json) //.splice(1, 20)
+	}
+
+	function sort_channels(server_id, cmp) {
+		let channels = channels_dict[server_id]
+		channels.sort(cmp);
+		server_list = server_list
+	}
+
+	function sort_channels__name(server) {
+		let sort_func = function(a, b){return a.name.localeCompare(b.name)}
+		return sort_channels(server, sort_func)
+	}
+	function sort_channels__messages_last_hour(server) {
+		let sort_func = function(a, b){return b.messages_last_hour-a.messages_last_hour}
+		return sort_channels(server, sort_func)
+	}
+	function sort_channels__last_message(server) {
+		let sort_func = function(a, b){return new Date(b.last_message.date)-new Date(a.last_message.date)}
+		return sort_channels(server, sort_func)
+	}
+	function sort_channels__total_messages(server) {
+		let sort_func = function(a, b){return b.total_messages-a.total_messages}
+		return sort_channels(server, sort_func)
+	}
+
+
+	async function get_channels(server_id) {
+		if (!(server_id in channels_dict)) {
+			let channels = await list_channels(server_id)
+			channels.sort(function(a, b){return a.name.localeCompare(b.name)})
+			channels_dict[server_id] = channels
+		}
+
+		return channels_dict[server_id]
+	}
+
+	async function init() {
+		server_list = await list_servers()
+	}
+	onMount(init)
+
+</script>
+​
+
+<main>
+	<h1>Portal Radar</h1>
+	<ul class="server_list">
+	{#each server_list as s, i}
+		<li class="server_item">
+			<div class="server_name">
+				{s.name}
+			</div>
+			<div class="server_stats">
+				<div>
+					{s.total_members} members
+				</div>
+				<div>
+					{s.channel_count} channels
+				</div>
+				<div>
+					{s.total_messages} messages
+				</div>
+			</div>
+			<div class="server_growth">
+				<b>Member Growth</b>
+				<div>
+					+{s.members_joined_last_24h} in the last day
+				</div>
+				<div>
+					+{s.members_joined_per_day_avg.toFixed(2)} every day (avg)
+				</div>
+			</div>
+			<div class="server_activity">
+				<div>
+					<b>Activity</b>
+				</div>
+				<time datetime="{s.last_message.date}">
+				Last message: {s.last_message.date_text}
+				</time>
+				<div>
+					{s.messages_last_hour} messages in the last hour
+				</div>
+			</div>
+			<div class="server_activity_graphs">
+				<Graph type="mph_by_dow" data="{s.mph_by_dow}" title="Messages by Day of Week"/>
+				<Graph type="mph_by_hod" data="{s.mph_by_hod}" title="Messages by Hour of Day (UTC)" show_x_axis=true/>
+			</div>
+			<div class="server_channel_list">
+				{#await get_channels(s.id)}
+					<p>...waiting for channels...</p>
+				{:then channel_list}
+					<div class="sort_menu">
+						Sort by:
+						<button on:click={() => sort_channels__name(s.id)}>
+							Name
+						</button>
+						<button on:click={() => sort_channels__last_message(s.id)}>
+							Last message
+						</button>
+						<button on:click={() => sort_channels__messages_last_hour(s.id)}>
+							Messages in the last hour
+						</button>
+						<button on:click={() => sort_channels__total_messages(s.id)}>
+							Total messages
+						</button>
+					</div>
+					<ul>
+					{#each channel_list as c, i}
+						<li class="channel_item">
+							<div class="channel_name">
+								<a href="https://discord.com/channels/{s.id}/{c.id}" target="_blank">
+									{c.name}
+								</a>
+							</div>
+
+							<div class="channel_stats">
+								<div>
+									{c.total_messages} messages
+								</div>
+								<div>
+									{c.messages_last_hour} in the last hour
+								</div>
+								<div>
+									<time datetime="{c.last_message.date}">
+									Last message: {c.last_message.date_text}
+									</time>
+								</div>
+							</div>
+
+							<div class="server_activity_graphs">
+								<Graph type="mph_by_dow" data="{c.mph_by_dow}" />
+								<Graph type="mph_by_hod" data="{c.mph_by_hod}" show_x_axis=true/>
+							</div>
+
+						</li>
+					{/each}
+					</ul>
+				{:catch error}
+					Error loading channels
+				{/await}
+			</div>
+		</li>
+	{/each}
+	</ul>
+</main>
+
+<style>
+.server_name {
+	grid-area: server;
+	font-weight: bold;
+	font-size: 20px;
+}
+.server_stats {
+	grid-area: stats;
+}
+.server_growth {
+	grid-area: growth;
+}
+.server_activity {
+	grid-area: activity;
+}
+.server_activity_graphs {
+	grid-area: activity_graphs;
+	display: flex;
+}
+.server_channel_list {
+	grid-area: channels
+}
+
+.mph_by_dow {
+	width: 400px;
+	margin: auto;
+}
+.mph_by_hod {
+	width: 400px;
+	margin: auto;
+}
+
+.server_item {
+	display: grid;
+	grid-template-columns: 200px 200px 900px;
+
+	grid-template-areas:
+		"server growth   activity_graphs"
+		"stats  activity activity_graphs"
+		"channels channels channels";
+
+	padding: 10px;
+	border: 3px solid silver;
+	border-radius: 10px;
+}
+
+.channel_name {
+	grid-area: name;
+	font-weight: bold;
+	font-size: 18px;
+}
+
+.channel_stats {
+	grid-area: stats;
+}
+
+.channel_item {
+	display: grid;
+	grid-template-columns: 60px 300px 800px;
+
+	grid-template-areas:
+		"blank name   activity_graphs"
+		"blank stats  activity_graphs"
+	;
+
+	margin-top: 10px;
+	padding: 10px;
+	background: #eee;
+	border-bottom: 1px solid silver;
+	/* border-radius: 10px; */
+}
+
+.sort_menu {
+	margin-top: 40px;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+main {
+	text-align: center;
+	padding: 1em;
+	max-width: 240px;
+	margin: 0 auto;
+}
+
+h1 {
+	color: #ff3e00;
+	text-transform: uppercase;
+	font-size: 4em;
+	font-weight: 100;
+}
+
+@media (min-width: 640px) {
+	main {
+		max-width: none;
+	}
+}
+</style>
+<link rel="stylesheet" href="uPlot/uPlot.min.css">
---
+++ client/src/Graph.svelte
@@ -0,0 +1,92 @@
+<script>
+    export let type = ""
+    export let data = {}
+    export let title = ""
+    export let height = 100
+    export let show_x_axis = false
+
+	import {uPlot} from '../public/uPlot/uPlot.iife.min.js'
+    import { afterUpdate } from 'svelte';
+
+	let data_elem
+
+    let DAYS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+
+	let created_graph = false
+
+	afterUpdate(() => {
+        if (data_elem && !created_graph) {
+            let d = extract_data(data_elem)
+            if (type == "mph_by_dow") {
+                createGraph_MphByDow(d, data_elem)
+            } else if (type == "mph_by_hod") {
+                createGraph_MphByHod(d, data_elem)
+			}
+			created_graph = true
+        }
+    })
+
+	function extract_data(elem) {
+		let data = JSON.parse(elem.attributes['data-series'].nodeValue)
+		let data2 = [[], []]
+		for (let i of data) {
+			data2[0].push(parseInt(i[0]))
+			data2[1].push(i[1])
+		}
+		return data2
+    }
+
+	function createGraph_MphByDow(data, elem) {
+		return createGraph(data, elem, {
+				label: "Day of Week",
+				value: (self, rawValue) => DAYS[rawValue]
+			}, {
+			title: title,
+			width: 400,
+			height: height,
+		})
+	}
+	function createGraph_MphByHod(data, elem) {
+		return createGraph(data, elem, {
+				label: "Hour of Day",
+				value: (self, rawValue) => rawValue
+			}, {
+			title: title,
+			width: 400,
+			height: height,
+		})
+	}
+
+	function createGraph(data, elem, xaxis, ext_opts) {
+		let opts = {
+            ...ext_opts,
+            axes: [
+                { show: show_x_axis, },
+                { show: true, }
+            ],
+			series: [
+				xaxis,
+				{
+					show: true,
+					spanGaps: true,
+					label: "Messages per hour (Avg)",
+					value: (self, rawValue) => rawValue.toFixed(2),
+					width: 1,
+					stroke: "#03a9f4",
+					fill: "#b3e5fc",
+				}
+			],
+			scales: { "x": { time: false } }
+		};
+
+		let uplot = new uPlot(opts, data, elem);
+	}
+
+
+</script>
+
+<div>
+	<slot>
+        <div class="graph {type}" bind:this={data_elem} data-series="{JSON.stringify(Object.entries(data))}"></div>
+	</slot>
+</div>
