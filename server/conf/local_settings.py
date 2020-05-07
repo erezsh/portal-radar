f5e1785c8d9bae9a340c04a83214d23169886018
devinMcArthur <assortedgerm@gmail.com>
2020-05-07 13:06:14 -0600

    config 1.1

---
+++ server/conf/local_settings.py
@@ -0,0 +1,8 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.path.join(BASE_DIR, 'db.postgresql'),
+    }
+}
+
+CORS_ORIGIN_ALLOW_ALL = True
