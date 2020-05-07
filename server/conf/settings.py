6023608dd4d255f317d65f6e42d07eeefde9424c
devinMcArthur <assortedgerm@gmail.com>
2020-05-07 13:11:24 -0600

    config 1.3

--- server/conf/settings.py
+++ server/conf/settings.py
@@ -79,10 +79,11 @@ WSGI_APPLICATION = 'conf.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.path.join(BASE_DIR, 'db.postgresql'),
     }
 }
+
 
 
 # Password validation
